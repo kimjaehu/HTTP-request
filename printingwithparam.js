583fa06580d4cf2670bc9d33b1ae115f0fd62375
Jay Kim <kimjaehu@gmail.com>
2019-02-11 17:20:14 +0000

    finished Step 3 - Printing with Parameters

--- printingwithparam.js
+++ printingwithparam.js
@@ -1,19 +1,18 @@
 function getAndPrintHTML (options) {
 
         var https = require('https');
-        
         var data = '';
 
         // notice that https.get takes a callback with one parameter -
         // response, which is a Stream that represents the HTTP response
-        https.get(requestOptions, function(response) {
+        https.get(options, function(response) {
             
             // Set the encoding to be utf8. 
             response.setEncoding('UTF8');
     
             // the callback is invoked when a `data` chunk is received
             response.on('data', function(chunk) {
-            data += (`${chunk.toString()}`);
+            data += (`${chunk}`);
             });
     
             response.on('end',function() {
@@ -25,10 +24,10 @@ function getAndPrintHTML (options) {
             });
         });
 }
-
-    getAndPrintHTML();
   
 var requestOptions = {
     host: 'sytantris.github.io',
     path: '/http-examples/step3.html'
 };
+
+getAndPrintHTML(requestOptions);
