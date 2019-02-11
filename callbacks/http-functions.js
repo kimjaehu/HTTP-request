14e0a30b44dac9bb3c550cad93ba2261167c78a5
Jay Kim <kimjaehu@gmail.com>
2019-02-11 18:01:29 +0000

    print-uppercase finished

---
+++ callbacks/http-functions.js
@@ -0,0 +1,27 @@
+module.exports = function getHTML (options, callback) {
+    /* Your code here */
+    var https = require('https');
+    var data = '';
+
+    // notice that https.get takes a callback with one parameter -
+    // response, which is a Stream that represents the HTTP response
+    https.get(options, function(response) {
+        
+        // Set the encoding to be utf8. 
+        response.setEncoding('UTF8');
+
+        // the callback is invoked when a `data` chunk is received
+        response.on('data', function(chunk) {
+        data += (`${chunk}`);
+        });
+
+        response.on('end',function() {
+            callback(data);
+        });
+
+        response.on('error', function(err) {
+        console.log(err.stack);
+        });
+    });
+
+};
