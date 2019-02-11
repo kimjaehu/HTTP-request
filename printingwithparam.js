6fdc74e30fdaa350b71944b2f67d92e5505fbd63
Jay Kim <kimjaehu@gmail.com>
2019-02-11 16:54:00 +0000

    finished Step 1 - Printing in Chunks

---
+++ printingwithparam.js
@@ -0,0 +1,34 @@
+function getAndPrintHTML (options) {
+
+        var https = require('https');
+        var data = '';
+    
+        // notice that https.get takes a callback with one parameter -
+        // response, which is a Stream that represents the HTTP response
+        https.get(requestOptions, function(response) {
+            
+            // Set the encoding to be utf8. 
+            response.setEncoding('UTF8');
+    
+            // the callback is invoked when a `data` chunk is received
+            response.on('data', function(chunk) {
+            data += (`${chunk.toString()}`);
+            });
+    
+            response.on('end',function() {
+            console.log(data);
+            });
+    
+            response.on('error', function(err) {
+            console.log(err.stack);
+            });
+        });
+    }
+
+    getAndPrintHTML();
+  
+  var requestOptions = {
+    host: 'sytantris.github.io',
+    path: '/http-examples/step3.html'
+  };
+  
--- printwithbufferedstrings.js
+++ printwithbufferedstrings.js
@@ -1,11 +1,11 @@
-function getAndPrintHTMLChunks () {
+function getAndPrintHTML () {
 
     var https = require('https');
     var data = '';
 
     var requestOptions = {
-      host: 'sytantris.github.io',
-      path: '/http-examples/step1.html'
+        host: 'sytantris.github.io',
+        path: '/http-examples/step2.html'
     };
 
     // notice that https.get takes a callback with one parameter -
@@ -17,11 +17,11 @@ function getAndPrintHTMLChunks () {
 
         // the callback is invoked when a `data` chunk is received
         response.on('data', function(chunk) {
-         console.log(`${chunk}${`\n`}`);
+        data += (`${chunk.toString()}`);
         });
 
         response.on('end',function() {
-
+        console.log(data);
         });
 
         response.on('error', function(err) {
@@ -29,4 +29,4 @@ function getAndPrintHTMLChunks () {
         });
     });
 }
-getAndPrintHTMLChunks();
+getAndPrintHTML();
